d376bb4e4facde5331d5046d775587d99d4dc276
Bryan Chase <bryanchase@yahoo.com>
2025-10-20 20:57:30 -0500

    Add admin page

--- src/components/forms/MortgageCalculator.js
+++ src/components/forms/MortgageCalculator.js
@@ -135,15 +135,15 @@ export default function MortgageCalculator({ form, updateForm }) {
               <div className="space-y-1 text-sm text-gray-700">
                 <div className="flex justify-between">
                   <span>Loan Amount:</span>
-                  <span>${loanAmount.toFixed(2).toLocaleString()}</span>
+                  <span>${loanAmount.toFixed(0).toLocaleString()}</span>
                 </div>
                 <div className="flex justify-between">
                   <span>Monthly P&I:</span>
-                  <span>${monthlyPayment.toLocaleString()}</span>
+                  <span>${monthlyPayment.toFixed(0).toLocaleString()}</span>
                 </div>
                 <div className="flex justify-between">
                   <span>Total Interest:</span>
-                  <span>${((monthlyPayment * (form.years || 0) * 12) - loanAmount).toFixed(2).toLocaleString()}</span>
+                  <span>${((monthlyPayment * (form.years || 0) * 12) - loanAmount).toFixed(0).toLocaleString()}</span>
                 </div>
               </div>
             </div>
